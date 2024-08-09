44eb7ed4d374985162bc0fbc90b9c445eee2a16a
SYEONIH <syeonih@naver.com>
2024-08-09 12:19:22 +0900

    main, loc 만 테일윈드 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/pages/Home.jsx","./src/pages/MyLocationList.jsx",],
   theme: {
     extend: {},
   },
